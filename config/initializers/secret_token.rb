c4ff47e5f5d13bc23b111dbbe19dc09921a9a461
Universidade Federal do Ceara <ufc@ufc-nagios.(none)>
2013-11-27 15:58:43 -0300

    Enviando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docdig::Application.config.secret_token = '445972fb30f489805e53bae2f1dc815daf4de3c95724c7dce1124229f78ad0fe93139a665b9b0aa9933c8da40b0034f35f5121a89d2bc64c7f63ab1a78cca723'
